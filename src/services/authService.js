6ab10e7ff6f4cb2f22c28a122af5b8c651be8367
matos17igor <igorm17_pvp@hotmail.com>
2026-02-11 15:39:47 -0300

    valida senha e cria token para o usuario logado

---
+++ src/services/authService.js
@@ -0,0 +1,26 @@
+const userRepository = require("../repositories/userRepository");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+async function login(email, password) {
+  const user = await userRepository.findByEmail(email);
+
+  if (!user) {
+    throw new Error("Email ou senha invalidos!");
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (!passwordMatch) {
+    throw new Error("Email ou senha invalidos!");
+  }
+
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    // de onde vem o user.id?
+    expiresIn: "1h",
+  });
+
+  return { token };
+}
+
+module.exports = { login };
