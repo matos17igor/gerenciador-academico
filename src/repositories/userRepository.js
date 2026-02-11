2c469813cbc817d78fd995a042623b25e822a16d
matos17igor <igorm17_pvp@hotmail.com>
2026-02-11 00:29:04 -0300

    cria funcoes de dados do repository

---
+++ src/repositories/userRepository.js
@@ -0,0 +1,29 @@
+const db = require("../config/database");
+
+async function create(user) {
+  const sql = `
+        INSERT INTO users(name, email, password)
+        VALUES(?, ?, ?)
+    `;
+
+  const [result] = await db.execute(sql, [
+    user.name,
+    user.email,
+    user.password,
+  ]);
+
+  return result.insertId;
+}
+
+async function fyndByEmail(email) {
+  const sql = `
+        SELECT * FROM users
+        WHERE email = ?
+    `;
+
+  const [rows] = await db.execute(sql, [email]);
+
+  return rows[0];
+}
+
+module.exports = { create, fyndByEmail };
