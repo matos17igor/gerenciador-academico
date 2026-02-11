153c08ece78cbb1fb283c222a016a7086147a43d
matos17igor <igorm17_pvp@hotmail.com>
2026-02-11 15:52:04 -0300

    adiciona rota /auth

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 const db = require("./config/database");
 const PORT = 3000;
+
 const userRoutes = require("./routes/userRoutes");
+const authRoutes = require("./routes/authRotes");
 
 app.use(express.json());
 
 app.use("/users", userRoutes);
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
